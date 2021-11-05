1d05669d74d7bc22287315d6df219ec20899e208
dbonach <deivitlopes@gmail.com>
2021-11-05 19:58:03 -0300

    Add script to login page

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    window.scroll(0, 0)
+  }
+
+  login() {
+    this.auth.login(this.userLogin).subscribe((res: UserLogin) => {
+      this.userLogin = res
+
+      environment.token = this.userLogin.token
+      environment.name = this.userLogin.name
+      environment.photoUri = this.userLogin.photoUri
+      environment.id = this.userLogin.id
+
+      this.router.navigate(['/inicio'])
+    }, err => {
+      if (err.status == 500) {
+        alert("Usuario ou senha estão incorretos!")
+      }
+    })
   }
 
 }
