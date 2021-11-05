310f1a0da896bb9888046ebe6b2d86423d7d8572
dbonach <deivitlopes@gmail.com>
2021-11-05 19:57:45 -0300

    Add script to registration page

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User()
+  password: string
+  type: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
+  }
+
+  checkPassword(event: any) {
+    this.password = event.target.value
+  }
+
+  userType(event: any) {
+    this.type = event.target.value
+  }
+
+  registration() {
+    this.user.userType = this.type
+
+    if (this.user.hashcode !== this.password) {
+      alert('As senhas são diferentes.')
+    } else {
+      this.authService.registration(this.user).subscribe((res: User) => {
+        this.user = res
+        alert('Usuario cadastrado com sucesso!')
+        this.router.navigate(['/'])
+      }, err => {
+        if (err.status == 500) {
+          alert('Usuário ou senha estão incorretos!')
+        }
+      })
+    }
   }
 
 }
