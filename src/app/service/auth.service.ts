96b7649a00ff822e3c18bac776ab487e43cfc31f
dbonach <deivitlopes@gmail.com>
2021-11-05 19:59:20 -0300

    Add authentication service

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { UserLogin } from '../model/UserLogin';
+import { User } from '../model/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  login(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>(`${environment.baseUrl}/user/login`, userLogin)
+  }
+
+  registration(user: User): Observable<User> {
+    return this.http.post<User>(`${environment.baseUrl}/user/registration`, user)
+  }
+}
