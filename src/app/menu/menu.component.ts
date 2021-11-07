638d7d8a47be83e452ed55ca47431e8f9afff8b5
dbonach <deivitlopes@gmail.com>
2021-11-06 21:22:00 -0300

    Dynamically load user photo and name in header

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  name = environment.name
+  photoUri = environment.photoUri
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  loggout() {
+    this.router.navigate(['/entrar'])
+
+    environment.token = ''
+    environment.name = ''
+    environment.photoUri = ''
+    environment.id = 0
   }
 
 }
